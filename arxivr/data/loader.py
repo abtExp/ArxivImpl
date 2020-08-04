b361be65cd4683fa3e9bf513618b34f145a11597
abtexp <anubhav.tiwari@rmgx.in>
2020-08-04 22:39:29 +0530

    base feeder and config updated

--- arxivr/data/loader.py
+++ arxivr/data/loader.py
@@ -1,64 +1,52 @@
-from tensorflow.keras.utils import Sequence
-# from torch import loader
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 
+from .feeder import FEEDER
 
-class DATA_LOADER():
-	def __init__(self, framework='keras', config=None, feeder=None):
-		if framework == 'keras':
-			loader = DATA_LOADER_KERAS
-		else:
-			loader = DATA_LOADER_TORCH
+class LOADER(tf.keras.utils.Sequence):
+    def __init__(self, config, feed_function, mode='train'):
+        self.config = config
+        self.feeder = FEEDER(config, feed_function)
 
-		return loader(config, feeder)
+        if mode == 'train':
+            self.batch_size = self.config.TRAIN_BATCH_SIZE
+        elif mode == 'valid':
+            self.batch_size = self.config.VALID_BATCH_SIZE
 
+        self.all_indices = self.feeder.get_indices()
 
-class DATA_LOADER_KERAS(Sequence):
-	def __init__(self, config, feeder=None):
-		self.feeder = feeder(config)
+        self.on_epoch_end()
 
-		super(DATA_LOADER_KERAS, self).__init__()
+    # Overwrite if a special kind of loader is needed, like one with synthetic data generation
+    def __getitem__(self, index):
+        indices = self.all_indices[0][index *
+                                    self.batch_size:(index+1)*self.batch_size]
 
-		if mode == 'train':
-			self.batch_size = self.vars.DETECTOR_TRAIN_BATCH_SIZE
-		elif mode == 'valid':
-			self.batch_size = self.vars.DETECTOR_VALID_BATCH_SIZE
+        features = [self.all_indices[0][i] for i in indices]
+        labels = [self.all_indices[1][i] for i in indices]
 
-		self.all_indices, self.dataset_len = self.loader.get_indices()
+        params = {
+            'features': features,
+            'labels': labels
+        }
 
-		self.on_epoch_end()
+        x, y = self.__data_generation(params)
 
-	# Overwrite if a special kind of loader is needed, like one with synthetic data generation
-	def __getitem__(self, index):
-		indices = self.all_indices[0][index*self.batch_size:(index+1)*self.batch_size]
+        return x, y
 
-		features = [self.all_indices[0][i] for i in indices]
-		labels = [self.all_indices[1][i] for i in indices]
+    def __len__(self):
+        length = int(np.floor(self.dataset_len / self.batch_size))
+        return length
 
-		params = {
-			'features':features,
-			'labels':labels
-		}
+    def __data_generation(self, params):
+        x, y = self.loader(self.config, self.mode, **params)
 
-		x, y = self.__data_generation(params)
+        return x, y
 
-		return x, y
-
-	def __len__(self):
-		length = int(np.floor(self.dataset_len / self.batch_size))
-		return length
-
-	def __data_generation(self, params):
-		x, y = self.loader(self.vars, self.mode, **params)
-
-		return x, y
-
-	def on_epoch_end(self):
-		for i in range(self.all_indices):
-			index = np.arrange(len(self.all_indices[i]))
-			np.random.shuffle(index)
-			self.indices.append(index)
-
-
-class DATA_LOADER_TORCH():
-	def __init__(self):
-		return
+    def on_epoch_end(self):
+        for i in range(self.all_indices):
+            index = np.arrange(len(self.all_indices[i]))
+            np.random.shuffle(index)
+            self.indices.append(index)
